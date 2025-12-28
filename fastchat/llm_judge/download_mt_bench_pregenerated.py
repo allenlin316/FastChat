df5b07bf2efb233d9a689b1ea90688b97fe8ce10
allenlin316 <allenlin316@gmail.com>
2025-12-28 03:03:56 +0000

    update download huggingface dataset

--- fastchat/llm_judge/download_mt_bench_pregenerated.py
+++ fastchat/llm_judge/download_mt_bench_pregenerated.py
@@ -6,15 +6,14 @@ import os
 from fastchat.utils import run_cmd
 
 filenames = [
-    "data/mt_bench/model_answers/llama2-bppo.jsonl",
-    "data/mt_bench/model_answers/llama2-ours-outliers-bppo.jsonl",
-    "data/mt_bench/model_answers/llama2-ours-outliers-qlora.jsonl",
-    "data/mt_bench/model_answers/llama2-ours-outliers-smart.jsonl",
-    "data/mt_bench/model_answers/llama2-qlora.jsonl",
-    "data/mt_bench/model_answers/gpt-4.1-mini.jsonl",
+    "data/mt_bench/model_answer/llama2-bppo.jsonl",
+    "data/mt_bench/model_answer/llama2-ours-outliers-bppo.jsonl",
+    "data/mt_bench/model_answer/llama2-ours-outliers-qlora.jsonl",
+    "data/mt_bench/model_answer/llama2-ours-outliers-smart.jsonl",
+    "data/mt_bench/model_answer/llama2-qlora.jsonl",
+    "data/mt_bench/model_answer/gpt-4.1-mini.jsonl",
     "data/mt_bench/model_judgment/gpt-4.1-mini_single.jsonl"
 ]
-
 
 if __name__ == "__main__":
     prefix = "https://huggingface.co/spaces/allenlin316/mt-bench-gpt4_1/resolve/main/"
